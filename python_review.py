853ac30f049c5e312f4162f7872bfbe3e2e09d3b
Jaritzi Lopez Martinez <jaritzilopezmartinez@Jaritzis-MacBook-Air.local>
2023-02-10 10:35:07 -0600

    inital commit

---
+++ python_review.py
@@ -0,0 +1,27 @@
+print ("hello world")
+
+# this is a single line comment
+
+"""
+multi line comment
+"""
+
+print("\n\n\n\n")
+
+number_1 = int(input("Input a number: "))
+number_2 = int(input("Input another number: "))
+total = number_1 + number_2
+print(total)
+
+if total > 10:
+	print("BOOOOOO!")
+elif total <= 9:
+	print("BOO!")
+else: # must be the value 10
+	print("your number is 10!")
+
+print("\n\n\n\n")
+
+for i in range(total):
+	print(1)
+
---
+++ starter_app/firstapp/__init__.py
@@ -0,0 +1,16 @@
+print("Hello World")
+
+"""
+
+all your text
+all your text
+all your text
+
+"""
+
+# single line comment
+
+number_1 = int(input("Input a number: "))
+number_2 = int(input("Input another number: "))
+total = int(number_1 + number_2)
+print(total)
